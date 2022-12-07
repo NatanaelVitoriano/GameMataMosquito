74111c266d8497ef22755290a43b270c97a009b8
NatanaelVitoriano <natanaelvitor88@gmail.com>
2022-12-07 13:51:10 -0300

    Iniciando Projeto game Mata Mosquito

---
+++ Readme.md
@@ -0,0 +1,2 @@
+App Game Mata Mosquito.
+Projeto da aula de JavaScript ultilizando os conceitos basicos de JS.
---
+++ game.js
@@ -0,0 +1,7 @@
+var altura = 0;
+var largura = 0;
+
+function ajustaTamanhoTelaGame() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+}
