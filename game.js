7ad4e3764291cb9fbd7ee7a8c8ae011f8ced67d9
NatanaelVitoriano <natanaelvitor88@gmail.com>
2022-12-08 13:30:40 -0300

    Adicionando cronometro.

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var altura = 0;
 var largura = 0;
 var vidas = 1;
+var tempo = 10;
 
 function ajustaTamanhoTelaGame() {
     altura = window.innerHeight;
@@ -8,6 +9,17 @@ function ajustaTamanhoTelaGame() {
 }
 
 ajustaTamanhoTelaGame();
+
+var cronometro = setInterval(function() {
+   tempo -= 1;
+   if(tempo < 0){
+    clearInterval(cronometro);
+    clearInterval(criaMosquito);
+    alert('Vitoria');
+   } else {
+    document.getElementById('cronometro').innerHTML = tempo;
+   }
+},1000);
 
 function posicaoRandomica() {
 
